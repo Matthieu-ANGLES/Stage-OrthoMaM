eb5ef34b819f6a9b91754fca61459da51ddc9b63
celinescornavacca <35933476+celinescornavacca@users.noreply.github.com>
2021-07-19 16:23:00 +0200

    Update downloadRelevantGCF.py

--- downloadRelevantGCF.py
+++ downloadRelevantGCF.py
@@ -3,6 +3,8 @@
 
 import os, sys
 from os import path as os_path
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 
 #------------------------------------------------------------------------------
 '''
@@ -16,8 +18,8 @@ def usage() :
     Usage : Launch error.
     'downloadRalevantGCF.py'
 
-    Use the 'assembly_summary.txt' retrieved at the bottom of the list at this address
-      - https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/
+    Use the 'assembly_summary_refseq.txt' retrieved at the bottom of the list at this address
+      - https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/assembly_summary.txt
 
     
     This module generate the "GCF.list" file used by 'addGenomes.py' in the GCF File.
@@ -75,8 +77,8 @@ def downloadGCF(summaryfile, outputGCFFolder, outFile) :
 
     extract = outFile.split(".")
     inexistentGCF_file = extract[0]+"_inexistent.list"  # making a path
-    GCFlist_File_GZ_extention = []
-    GCFlist_File = []
+
+    os.system( "cd "+outputGCFFolder)
 
     with open (summaryfile, 'r') as inputFile, open (outFile, 'w') as GCF_list, open (inexistentGCF_file, 'w') as inexistent_GCF_list :
         for line in inputFile :
@@ -85,69 +87,78 @@ def downloadGCF(summaryfile, outputGCFFolder, outFile) :
             genomeInfo = line.strip().split("\t") # recovery of the differents fields of interest
             genomeCategory = genomeInfo[4].strip()
             genometaxID = genomeInfo[5].strip()
-            taxonName = genomeInfo[7].strip().replace(" ","_")
-            if genometaxID in taxonIDtoClassif.keys(): # condition in case of no classification for a taxon
-                taxonClassif = taxonIDtoClassif[genometaxID]
-            else :
-                taxonClassif = "NA"
-            genomeFTPFolder = genomeInfo[19].strip() # FTP link of index files of genomes
-            tmp = genomeFTPFolder.split("/")
-            GCF_CDS_URL = genomeFTPFolder + "/" + tmp[-1] + "_cds_from_genomic.fna.gz"
-            GCF_Prot_URL = genomeFTPFolder + "/" + tmp[-1] + "_protein.faa.gz"
-            GCF_RNA_URL = genomeFTPFolder + "/" + tmp[-1] + "_rna.fna.gz"
-            GCF_Feature_Table_URL = genomeFTPFolder + "/" + tmp[-1] + "_feature_table.txt.gz"
-            GCF_RNA_GBFF_URL = genomeFTPFolder + "/" + tmp[-1] + "_rna.gbff.gz  "
 
             if (genomeCategory == "representative genome") or (genomeCategory == "reference genome") :
-                GCF_CDS_File = tmp[-1] + "_cds_from_genomic.fna"
-                GCF_Prot_File = tmp[-1] + "_protein.faa" 
-                GCF_RNA_File = tmp[-1] + "_rna.fna"
-                GCF_Feature_Table_File = tmp[-1] + "_feature_table.txt"
-                GCF_RNA_GBFF_File = tmp[-1] + "_rna.gbff"
-                cmd1 = "cd "+outputGCFFolder+"; wget "+GCF_CDS_URL+"; gzip -d "+GCF_CDS_File
-                cmd2 = "cd "+outputGCFFolder+"; wget "+GCF_Prot_URL+"; gzip -d "+GCF_Prot_File
-                cmd3 = "cd "+outputGCFFolder+"; wget "+GCF_RNA_URL+"; gzip -d "+GCF_RNA_File
-                # other style possible : cmd3 = f"cd {outputGCFFolder}; wget {GCF_RNA_URL}; gzip -d {GCF_RNA_File}"
-                cmd4 = "cd "+outputGCFFolder+"; wget "+GCF_Feature_Table_URL+"; gzip -d "+GCF_Feature_Table_File
-                cmd5 = "cd "+outputGCFFolder+"; wget "+GCF_RNA_GBFF_URL+"; gzip -d "+GCF_RNA_GBFF_File
-                os.system(cmd1)
-                #rescmd1 = os.popen(cmd1).readlines()
-                os.system(cmd2)
-                #rescmd2 = os.popen(cmd2).readlines()               
-                os.system(cmd3)
-                #rescmd3 = os.popen(cmd3).readlines()
-                os.system(cmd4)
-                #rescmd4 = os.popen(cmd4).readlines()
-                os.system(cmd5)
-                #rescmd5 = os.popen(cmd5).readlines()
+            
+                taxonName = genomeInfo[7].strip().replace(" ","_")
+            	
+                if genometaxID in taxonIDtoClassif.keys(): # condition in case of no classification for a taxon
+                	taxonClassif = taxonIDtoClassif[genometaxID]
+                else :
+                	taxonClassif = "NA"
+                
+                genomeFTPFolder = genomeInfo[19].strip() # FTP link of index files of genomes
+                tmp = genomeFTPFolder.split("/")
+                GCF_CDS_URL = genomeFTPFolder + "/" + tmp[-1] + "_cds_from_genomic.fna.gz"
+                GCF_Prot_URL = genomeFTPFolder + "/" + tmp[-1] + "_protein.faa.gz"
+                GCF_RNA_URL = genomeFTPFolder + "/" + tmp[-1] + "_rna.fna.gz"
+                GCF_Feature_Table_URL = genomeFTPFolder + "/" + tmp[-1] + "_feature_table.txt.gz"
+                GCF_RNA_GBFF_URL = genomeFTPFolder + "/" + tmp[-1] + "_rna.gbff.gz  "
 
-                GCFlist_File_GZ_extention.append(GCF_CDS_File+".gz")
-                GCFlist_File_GZ_extention.append(GCF_Prot_File+".gz")
-                GCFlist_File_GZ_extention.append(GCF_RNA_File+".gz")
-                GCFlist_File_GZ_extention.append(GCF_Feature_Table_File+".gz")
-                GCFlist_File_GZ_extention.append(GCF_RNA_GBFF_File+".gz")
-
-                GCFlist_File.append(GCF_CDS_File)
-                GCFlist_File.append(GCF_Prot_File)
-                GCFlist_File.append(GCF_RNA_File)
-                GCFlist_File.append(GCF_Feature_Table_File)
-                GCFlist_File.append(GCF_RNA_GBFF_File)
+                urls_to_get = []
+                urls_to_get.append(GCF_CDS_URL)
+                urls_to_get.append(GCF_Prot_URL)
+                urls_to_get.append(GCF_RNA_URL)
+                urls_to_get.append(GCF_Feature_Table_URL)
+                urls_to_get.append(GCF_RNA_GBFF_URL)
 
 
-                # Vérification GCF presence
                 GCF_Ok = True
-                for nameFile in GCFlist_File :
-                    cmdCheck = "cd "+outputGCFFolder+";find -name "+nameFile
-                    os.system(cmdCheck)
-                    rescmdCheck = os.popen(cmdCheck).readlines()
-                    if rescmdCheck == '':
-                        # add in inexistant_GCF.list
-                        inexistent_GCF_list.write(taxonName+"\t"+ genomeFTPFolder+"\t"+ GCF_CDS_File+"\t"+GCF_Prot_File+"\t"+GCF_RNA_File+"\t"+GCF_Feature_Table_File+"\t"+GCF_RNA_GBFF_File+"\t"+genometaxID+"\t"+taxonClassif+"\n")
+
+                for url in urls_to_get:
+                    print("Web site " + url)
+                    try:
+                        req = urlopen(url)
+                    except HTTPError as e:
+                        print('The server couldn\'t fulfill the request.', flush=True)
+                        print('Error code: ', e.code, flush=True)
                         GCF_Ok = False
                         break
-                # add in GCF.list
+                    except URLError as e:
+                        print('We failed to reach a server.', flush=True)
+                        print('Reason: ', e.reason, flush=True)
+                        GCF_Ok = False
+                        break
+                    except ValueError as e:
+                        print('Invalid ftp url.', flush=True)
+                        GCF_Ok = False
+                        break
+                    else:
+                        print("All good", flush=True)
+                          
                 if GCF_Ok == True :
+                    GCF_CDS_File = tmp[-1] + "_cds_from_genomic.fna"
+                    GCF_Prot_File = tmp[-1] + "_protein.faa" 
+                    GCF_RNA_File = tmp[-1] + "_rna.fna"
+                    GCF_Feature_Table_File = tmp[-1] + "_feature_table.txt"
+                    GCF_RNA_GBFF_File = tmp[-1] + "_rna.gbff"
+                    cmd1 = "cd "+outputGCFFolder+"; wget "+GCF_CDS_URL+"; gzip -d "+GCF_CDS_File
+                    cmd2 = "cd "+outputGCFFolder+"; wget "+GCF_Prot_URL+"; gzip -d "+GCF_Prot_File
+                    cmd3 = "cd "+outputGCFFolder+"; wget "+GCF_RNA_URL+"; gzip -d "+GCF_RNA_File
+                    cmd4 = "cd "+outputGCFFolder+"; wget "+GCF_Feature_Table_URL+"; gzip -d "+GCF_Feature_Table_File
+                    cmd5 = "cd "+outputGCFFolder+"; wget "+GCF_RNA_GBFF_URL+"; gzip -d "+GCF_RNA_GBFF_File
+                    os.system(cmd1)
+                    os.system(cmd2)
+                    os.system(cmd3)
+                    os.system(cmd4)
+                    os.system(cmd5)
+
                     GCF_list.write(taxonName+"\t"+ GCF_CDS_File+"\t"+GCF_Prot_File+"\t"+GCF_RNA_File+"\t"+GCF_Feature_Table_File+"\t"+GCF_RNA_GBFF_File+"\t"+genometaxID+"\t"+taxonClassif+"\n")
+                    print('Taxon with required files: ', taxonName + "\t"+genometaxID+"\t"+taxonClassif+"\n", flush=True)
+                else :
+                    inexistent_GCF_list.write(taxonName+"\t"+ genomeFTPFolder+"\t"+genometaxID+"\t"+taxonClassif+"\n")
+                    print('Taxon without required files: ', taxonName +"\t"+genometaxID+"\t"+taxonClassif+"\n", flush=True)
+
 
 
 #------------------------------------------------------------------------------
